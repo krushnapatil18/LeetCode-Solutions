c084a936e6a4685b6bebd80c800f7625de7e6d3d
Krushna Patil <krushanapatil3@gmail.com>
2025-03-03 23:41:47 +0530

    Time: 9 ms (94.66%), Space: 56.8 MB (52.91%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class Solution {
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // Splitting the linked list into two halves
+        ListNode slow = head, fast = head, prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prev.next = null; // Break the link between two halves
+
+        // Sorting each half recursively
+        ListNode left = sortList(head);
+        ListNode right = sortList(slow);
+
+        // Merging the sorted halves
+        return mergeLists(left, right);
+    }
+
+    private ListNode mergeLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+
+        // Attaching the remaining nodes
+        if (l1 != null) {
+            current.next = l1;
+        }
+        if (l2 != null) {
+            current.next = l2;
+        }
+
+        return dummy.next;
+    }
+}
